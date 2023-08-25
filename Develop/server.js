2db64e1433bc3af0b1576d158d0d48c3d730dcc9
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-25 16:04:02 -0700

    Comments and switching (port) to (PORT)

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const htmlRoutes = require("./routes/html-route");
 const apiRoutes = require("./routes/api-route");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -11,9 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Middleware for API routes
 app.use("/api", apiRoutes);
+
+// Middleware for HTML routes
 app.use("/", htmlRoutes);
 
+// Start the server after setting up middleware and routes
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
